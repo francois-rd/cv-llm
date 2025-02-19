68e723c7dddc01b6035a43a7001686741effff7b
Francois Roewer-Despres <francois-rd@users.noreply.github.com>
2025-02-18 18:10:34 -0600

    Add a dummy transformer config.

--- launch/extract_dummy.bash
+++ launch/extract_dummy.bash
@@ -6,4 +6,8 @@ if [ -z "$LLM" ]
 then
   LLM=dummy
 fi
-launch extract llm="$LLM" implementation=DUMMY "$@"
+launch extract \
+  llm="$LLM" \
+  implementation=DUMMY \
+  --transformers-path transformers_cfgs/dummy_transformers.yaml \
+  "$@"
